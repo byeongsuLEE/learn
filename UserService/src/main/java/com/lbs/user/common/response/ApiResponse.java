e093027b073ee4b976ec0302c40c7533e6dbb8a3
이병수 <evil55@naver.com>
2025-03-02 23:09:50 +0900

    feat [유저서비스] : 공통 response, errorCode 구현 완료

---
+++ UserService/src/main/java/com/lbs/user/common/response/ApiResponse.java
@@ -0,0 +1,89 @@
+package com.lbs.user.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+/**
+ *
+ *
+ */
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"code", "message", "data"})
+public class ApiResponse<T> {
+
+    private final HttpStatus status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL) // data가 null이면 JSON에서 제외
+    private final T data;
+
+
+
+    /**
+     *
+     * @ 작성자   : 이병수
+     * @ 작성일   : 2025-03-02
+     * @ 설명     : 요청 성공 시 데이터
+     * @param message
+     * @param data
+     * @return 상태, 메시지, 데이터
+     *
+     */
+    public static <T> ApiResponse<T> success (HttpStatus status, String message, T data){
+        return new ApiResponse<>(status,message,data);
+    }
+
+    /**
+     *
+     * @ 작성자   : 이병수
+     * @ 작성일   : 2025-03-02
+     * @ 설명     : 요청 성공 시 메시지 없이 보내기
+     * @param status
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> ApiResponse<T> success (HttpStatus status,T data){
+        return new ApiResponse<>(status,null,data);
+    }
+
+    /**
+     * 요청 실패 시 에러 반환 메서드
+     * @ 작성자   : 이병수
+     * @ 작성일   : 2025-03-02
+     * @ 설명     :
+     * @param errorCode
+     * @return
+     * @param
+     */
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<T>(errorCode.getStatus(),errorCode.getMessage(),null);
+    }
+
+
+    /**
+     *  카프카로 Apireponse 결과 값을 보낼 떄 json 사용
+     * @ 작성자   : 이병수
+     * @ 작성일   : 2025-03-02
+     * @ 설명     : 데이터를 json으로 변환 함수
+     * @return
+     */
+    public String toJson() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return "{}";
+        }
+    }
+
+}
--- UserService/src/main/java/com/lbs/user/presentation/controller/UserController.java
+++ UserService/src/main/java/com/lbs/user/presentation/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.lbs.user.presentation.controller;
 
+import com.lbs.user.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.lbs.user.common.response.ApiResponse.success;
 
 /**
  * 작성자  : 이병수
@@ -18,9 +25,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/welcome")
-    public String gatewayConnectTest (){
+    public ResponseEntity<ApiResponse<String>> gatewayConnectTest (){
 
         log.info("들어왔나?");
-        return "success";
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,"테스트 성공"));
+
     }
 }
