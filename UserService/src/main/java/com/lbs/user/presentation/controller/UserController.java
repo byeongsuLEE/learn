51748c71633cc2d944417a62a8c497c8005bf11e
byeongsuLEE <75319125+byeongsuLEE@users.noreply.github.com>
2025-03-04 17:25:52 +0900

    feat [Gateway] : gateway config global,logging,custom filter 추가 및 discovery 연결

--- UserService/src/main/java/com/lbs/user/presentation/controller/UserController.java
+++ UserService/src/main/java/com/lbs/user/presentation/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.lbs.user.presentation.controller;
 
 import com.lbs.user.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,10 @@ import static com.lbs.user.common.response.ApiResponse.success;
 @RestController
 @RequestMapping
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public ResponseEntity<ApiResponse<String>> gatewayConnectTest (){
@@ -32,4 +37,22 @@ public class UserController {
                 .body(ApiResponse.success(HttpStatus.OK,"테스트 성공"));
 
     }
+    @GetMapping("/check")
+    public ResponseEntity<ApiResponse<String>> gatewayCustomFilterTest (){
+
+        log.info("customfilter check ");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,"customfilter success "));
+
+    }
+    @GetMapping("/port-check")
+    public ResponseEntity<ApiResponse<String>> gatewayPortCheck (){
+
+        log.info("customfilter check ");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,
+                        String.format("서버 포트는 : %s", env.getProperty("local.server.port"))));
+
+    }
+
 }
