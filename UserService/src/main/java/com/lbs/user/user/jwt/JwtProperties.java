376666aa1601e453213d3bbb2321b7c26963dcf9
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-05-13 01:53:55 +0900

    feat [User-Security] : naver oauth2 추가

--- UserService/src/main/java/com/lbs/user/user/jwt/JwtProperties.java
+++ UserService/src/main/java/com/lbs/user/user/jwt/JwtProperties.java
@@ -8,12 +8,14 @@ package com.lbs.user.user.jwt;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Getter
+@Setter
 public class JwtProperties {
     private String secret;
     private long accessExpirationTime;
--- UserService/src/main/java/com/lbs/user/user/security/CustomOauth2UserInfoService.java
+++ UserService/src/main/java/com/lbs/user/user/security/CustomOauth2UserInfoService.java
@@ -1,5 +1,7 @@
 package com.lbs.user.user.security;
 
+import com.lbs.user.user.domain.User;
+import com.lbs.user.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -11,7 +13,9 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,18 +25,41 @@ import java.util.Map;
  **/
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class CustomOauth2UserInfoService extends DefaultOAuth2UserService  {
+    private final OAuth2UserInfoMapperFactory oAuth2UserInfoMapperFactory;
+    private final UserService userService;
 
+    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        log.info("OAuth2 로그인 요청 : {}", userRequest.getClientRegistration().getClientId());
+        String clientId = userRequest.getClientRegistration().getRegistrationId();
+        log.info("OAuth2 로그인 요청 : {}", clientId);
         log.info("OAuth 로그인 : " + oAuth2User.getAttributes());
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        OAuth2UserInfoMapper clientMapper = oAuth2UserInfoMapperFactory.getOAuth2UserInfoMapper(clientId);
+        String id = clientMapper.getId(attributes);
+        String email = clientMapper.getEmail(attributes);
+        String name = clientMapper.getName(attributes);
+        String imageUrl = clientMapper.getImageUrl(attributes);
 
-        return oAuth2User;
+        //email로 userEntity 가져온다.-
+        User user = userService.readUserByEmail(email);
+        if (user == null) {
+            user  = User.builder()
+                    .imageUrl(imageUrl)
+                    .email(email)
+                    .name(name)
+                    .provider(clientId)
+                    .providerId(id)
+                    .roles(List.of("ROLE_USER"))
+                    .build();
+            userService.joinUser(user);
+        }
+        // 해당 정보를 가지고
+        return new CustomOauth2User(oAuth2User,user);
     }
 
 
