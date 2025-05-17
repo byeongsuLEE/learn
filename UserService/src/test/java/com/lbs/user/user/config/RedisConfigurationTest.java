c34e0f5a1d658357750336be177d4c6197f7b1c4
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-05-17 23:36:27 +0900

    feat [User-Security] : redis 환경 설정 및 연결

---
+++ UserService/src/test/java/com/lbs/user/user/config/RedisConfigurationTest.java
@@ -0,0 +1,19 @@
+package com.lbs.user.user.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedisConfigurationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
