a8340cd3bb800c59e7e46548486017f8f8e69a86
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-05-23 20:04:38 +0900

    feat [User-Micrometer]: 모니터링 설정

--- UserService/src/main/java/com/lbs/user/UserServiceApplication.java
+++ UserService/src/main/java/com/lbs/user/UserServiceApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
