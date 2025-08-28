bc155aabe23949b8f4ac2ee9c4073a0252772f23
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-08-29 03:36:53 +0900

    test : 로그인 배포 테스트

--- UserService/src/main/java/com/lbs/user/video/controller/VideoService.java
+++ UserService/src/main/java/com/lbs/user/video/controller/VideoService.java
@@ -3,7 +3,7 @@ package com.lbs.user.video.controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Service
+
 public interface VideoService {
     void uploadVideo(MultipartFile file);
 }
