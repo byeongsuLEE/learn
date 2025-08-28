a4475bba3cd3dcd167c89ffed3a5484108912119
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-08-28 15:06:15 +0900

    feat : video service 작성 중

---
+++ UserService/src/main/java/com/lbs/user/video/controller/VideoController.java
@@ -0,0 +1,38 @@
+package com.lbs.user.video.controller;
+
+import com.lbs.user.common.response.ApiResponse;
+import com.lbs.user.video.dto.request.VideoUploadDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 작성자  : lbs
+ * 날짜    : 2025-08-27
+ * 풀이방법
+ **/
+
+
+@Slf4j
+@RestController("/videos")
+@RequiredArgsConstructor
+public class VideoController {
+    private final VideoService videoService ;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse<String>> uploadVideo (@ModelAttribute VideoUploadDto videoUploadDto) {
+        log.info(videoUploadDto.toString());
+
+//        videoService.uploadVideo(videoUploadDto.videoFile());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(HttpStatus.CREATED,"동영상 업로드 완료"));
+    }
+
+}
