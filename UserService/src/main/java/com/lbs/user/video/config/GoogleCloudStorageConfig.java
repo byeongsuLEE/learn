8a9f195b83ffe6a964297a25b2dcff2f28dafb1f
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-09-30 07:03:55 +0900

    rollback : 배포 테스트 에러를 위한 구글 prod 재생성

--- UserService/src/main/java/com/lbs/user/video/config/GoogleCloudStorageConfig.java
+++ UserService/src/main/java/com/lbs/user/video/config/GoogleCloudStorageConfig.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 @Configuration
+@Profile("local") // 로컬 환경에서만 이 빈을 사
 public class GoogleCloudStorageConfig  {
     @Value("${google.cloud.storage.credentials.location}")
     private String credentialsLocation;
