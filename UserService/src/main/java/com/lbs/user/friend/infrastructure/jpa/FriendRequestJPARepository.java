9b9c0ebba503dcb83414007ef8ee7de57b3268b0
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-10-15 16:14:09 +0900

    feat : 유저 친구 중복 요청 시 데이터 중복 방지

--- UserService/src/main/java/com/lbs/user/friend/infrastructure/jpa/FriendRequestJPARepository.java
+++ UserService/src/main/java/com/lbs/user/friend/infrastructure/jpa/FriendRequestJPARepository.java
@@ -2,7 +2,11 @@ package com.lbs.user.friend.infrastructure.jpa;
 
 import com.lbs.user.user.infrastructure.entity.FriendEntity;
 import com.lbs.user.user.infrastructure.entity.FriendRequestEntity;
+import com.lbs.user.user.infrastructure.entity.FriendRequestStatus;
+import com.lbs.user.user.infrastructure.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 /**
  * 작성자  : lbs
@@ -12,5 +16,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface FriendRequestJPARepository extends JpaRepository<FriendRequestEntity, Long> {
+
+    Optional<FriendRequestEntity> findBySenderAndReceiverAndStatus(UserEntity sender, UserEntity receiver, FriendRequestStatus status);
 }
 
