010f53525b0cec5c32e5df7e32b9bb989e4a76e8
lb <75319125+byeongsuLEE@users.noreply.github.com>
2025-10-15 16:14:09 +0900

    feat : 유저 친구 중복 요청 시 데이터 중복 방지

--- UserService/src/main/java/com/lbs/user/friend/repository/FriendAdapterRepository.java
+++ UserService/src/main/java/com/lbs/user/friend/repository/FriendAdapterRepository.java
@@ -9,12 +9,14 @@ import com.lbs.user.friend.infrastructure.jpa.FriendJPARepository;
 import com.lbs.user.friend.infrastructure.jpa.FriendRequestJPARepository;
 import com.lbs.user.user.infrastructure.entity.FriendEntity;
 import com.lbs.user.user.infrastructure.entity.FriendRequestEntity;
+import com.lbs.user.user.infrastructure.entity.FriendRequestStatus;
 import com.lbs.user.user.infrastructure.entity.UserEntity;
 import com.lbs.user.user.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 작성자  : lbs
@@ -50,6 +52,8 @@ public class FriendAdapterRepository implements FriendRepository {
     public FriendRequest sendFriendRequest(FriendRequest friendRequest) {
 
 
+
+
         //user repository 에서 sender , receiver 가져오기
 
         UserEntity sender = userRepository.findById(friendRequest.getSenderId())
@@ -58,11 +62,21 @@ public class FriendAdapterRepository implements FriendRepository {
         UserEntity receiver = userRepository.findById(friendRequest.getReceiverId())
                 .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
 
-        FriendRequestEntity friendRequestEntity = FriendRequestEntity.createFriendRequestEntity(sender, receiver, friendRequest.getStatus());
+        Optional<FriendRequestEntity> bySenderAndReceiverAndStatus = friendRequestJPARepository.findBySenderAndReceiverAndStatus(sender, receiver, FriendRequestStatus.PENDING);
 
-        FriendRequestEntity savedEntity = friendRequestJPARepository.save(friendRequestEntity);
 
-        return savedEntity.entityToDomain();
+        return friendRequestJPARepository.findBySenderAndReceiverAndStatus(sender, receiver, FriendRequestStatus.PENDING)
+                 // 전에 보냈던 요청이 있다면 db에는 다시 저장하지 안하기
+                 .map(FriendRequestEntity::entityToDomain)
+                 // 이전의 보낸 요청이 없다면 데이터 베이스에 저장하기
+                .orElseGet(()->{
+                    FriendRequestEntity friendRequestEntity = FriendRequestEntity.createFriendRequestEntity(sender, receiver, friendRequest.getStatus());
+
+                    FriendRequestEntity savedEntity = friendRequestJPARepository.save(friendRequestEntity);
+
+                    return savedEntity.entityToDomain();
+                });
+
     }
 
     @Override
