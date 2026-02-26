75a31de967a9e0f3de0a87ecabed246408f4b9da
lb <75319125+byeongsuLEE@users.noreply.github.com>
2026-02-27 05:20:51 +0900

    fix : 도메인 evil55.shop -> evil55.cloud로 변경

--- UserService/src/main/java/com/lbs/user/card/controller/CardController.java
+++ UserService/src/main/java/com/lbs/user/card/controller/CardController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;ddddd
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 작성자  : lbs
